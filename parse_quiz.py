b3843fb41ddd00ed20d95698d3165fcd8611f30e
acostyle <acostyle.m.z@protonmail.com>
2021-10-19 11:57:03 +0600

    feat: add questions parsing

---
+++ parse_quiz.py
@@ -0,0 +1,10 @@
+def parse_quiz_file():
+    with open("1vs1200.txt", "r", encoding="KOI8-R") as file:
+        file_content = file.read()
+
+    quiz_file_content = file_content.split('\n\n')
+
+    questions = [question for question in quiz_file_content if question.startswith('Вопрос')]
+    answers = [answer for answer in quiz_file_content if answer.startswith('Ответ')]
+
+    return dict(zip(questions, answers))
