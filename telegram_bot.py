75bf6210892d1dd9e3f5e145f05b390487242e3c
acostyle <acostyle.m.z@protonmail.com>
2021-10-28 11:43:46 +0600

    feat: add handlers

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,17 +1,27 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram import ReplyKeyboardMarkup, replymarkup
+from functools import partial
+from queue import Queue
+from redis import Redis
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+from telegram import ReplyKeyboardMarkup
 import logging
 from environs import Env
 import random
+import json
+
 from parse_quiz import parse_quiz_file
 
 env = Env()
 env.read_env()
 
-TELEGRAM_TOKEN = env('TELEGRAM_API_TOKEN')
+TELEGRAM_TOKEN = env.str('TELEGRAM_API_TOKEN')
 CHAT_ID = env.int('CHAT_ID')
+REDIS_HOST = env.str('REDIS_HOST')
+REDIS_PORT = env.int('REDIS_PORT')
+REDIS_PASSWORD = env.str('REDIS_PASSWORD')
 
 QUIZ_CONTENT = parse_quiz_file()
+QUESTION = 0
+ANSWER = 1
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -23,23 +33,46 @@ def start(bot, update):
     """Send a message when the command /start is issued."""
     custom_keyboard = [['Новый вопрос', 'Сдаться'], ['Мой счет']]
     reply_markup = ReplyKeyboardMarkup(custom_keyboard)
-    bot.send_message(chat_id=CHAT_ID, reply_markup=reply_markup)
+    bot.send_message(
+        chat_id=CHAT_ID,
+        text='Я – бот для викторин',
+        reply_markup=reply_markup
+    )
+
+    return QUESTION
 
 
-def help(bot, update):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-
-def send_question(bot, update):
+def send_question(bot, update, db):
     """Send to the user question."""
+    question, answer = random.choice(list(QUIZ_CONTENT.items()))
+
+    db.set(update.effective_chat.id, json.dumps([question, answer]))
+    update.message.reply_text(question)
+
+    return ANSWER
+
+
+def check_answer(bot, update, db):
+    question_with_answer = db.get(update.effective_user.id).decode('utf-8')
+    
+    question, answer = json.loads(question_with_answer)
+    print(answer)   
 
     if update.message.text == 'Новый вопрос':
-        question = random.choice(list(QUIZ_CONTENT.keys()))
-        update.message.reply_text(question)
+        update.message.reply_text(
+            f'Вы не ответили на старый вопрос!\n{question}')
+    elif update.message.text == 'Сдаться':
+        update.message.reply_text(f'Правильный {answer}')
+        return QUESTION
+    elif update.message.text.lower() in answer.lower():
+        update.message.reply_text(f"Верно! {answer}")
+        return QUESTION
+    else:
+        update.message.reply_text(
+            "Ответ неверный! Попробуйте еще раз или сдайтесь!")
 
 
-def error(bot, update, error):
+def error(update, error):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, error)
 
@@ -47,16 +80,25 @@ def error(bot, update, error):
 def main():
     """Start the bot."""
     updater = Updater(TELEGRAM_TOKEN)
-
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
+    redis_db = Redis(
+        host=REDIS_HOST,
+        port=REDIS_PORT,
+        password=REDIS_PASSWORD,
+    )
 
-    dp.add_handler(MessageHandler(Filters.text, send_question))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            QUESTION: [MessageHandler(Filters.regex('^Новый вопрос$'), partial(send_question, db=redis_db))],
+            ANSWER: [MessageHandler(Filters.text, partial(check_answer, db=redis_db))],
+        },
+        fallbacks=[Filters.regex('^Всё$')]
+    )
 
+    dp.add_handler(conv_handler)
     dp.add_error_handler(error)
-
     updater.start_polling()
     updater.idle()
 
