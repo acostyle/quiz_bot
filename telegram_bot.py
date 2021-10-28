1e77063d7fe6a261dd44ba86c2c49f2141e9a8b3
acostyle <acostyle.m.z@protonmail.com>
2021-10-28 12:39:25 +0600

    chore: move all constants to separate file

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,23 +1,14 @@
 from functools import partial
-from queue import Queue
 from redis import Redis
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 import logging
-from environs import Env
 import random
 import json
 
 from parse_quiz import parse_quiz_file
+from constants import TELEGRAM_TOKEN, REDIS_HOST, REDIS_PASSWORD, REDIS_PORT, CHAT_ID
 
-env = Env()
-env.read_env()
-
-TELEGRAM_TOKEN = env.str('TELEGRAM_API_TOKEN')
-CHAT_ID = env.int('CHAT_ID')
-REDIS_HOST = env.str('REDIS_HOST')
-REDIS_PORT = env.int('REDIS_PORT')
-REDIS_PASSWORD = env.str('REDIS_PASSWORD')
 
 QUIZ_CONTENT = parse_quiz_file()
 QUESTION = 0
